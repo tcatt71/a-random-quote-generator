a6957513716b0cc48de941ea403334c84a6757d7
Troy Coffindaffer <61867285+tcatt71@users.noreply.github.com>
2022-09-17 04:53:44 -0700

    Delete additional quotes array

--- js/script.js
+++ js/script.js
@@ -29,14 +29,6 @@ const quotes = [
 ***/
 
 
-const quotes = [
-  { quote: "Whatever the mind of man can conceive and believe, it can achieve.", source: 'Napoleon Hill', citation: 'Think and Grow Rich', year: 1937 },
-  { quote: "You may be disappointed if you fail, but you are doomed if you don't try.", source: 'Beverly Sills' },
-  { quote: "I failed my way to success", source: 'Thomas Edison' },
-  { quote: "Many of life's failures are people who did not realize how close they were to success when they gave up.", source: 'Thomas A. Edison' },
-  { quote: "May you live all the days of your life.", source: 'Jonathan Swift' }
-];
-
 /***
  * click event listener for the print quote button
  * DO NOT CHANGE THE CODE BELOW!!
