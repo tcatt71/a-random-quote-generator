f4254ab53d78ef9abf960d98533ea9beac89d643
Troy Coffindaffer <61867285+tcatt71@users.noreply.github.com>
2022-09-17 04:51:53 -0700

    Update quotes array

--- js/script.js
+++ js/script.js
@@ -8,6 +8,13 @@ project 1 - A Random Quote Generator
 // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /** An Array of qoute objects. */
+const quotes = [
+  { quote: "Whatever the mind of man can conceive and believe, it can achieve.", source: 'Napoleon Hill', citation: 'Think and Grow Rich', year: 1937 },
+  { quote: "You may be disappointed if you fail, but you are doomed if you don't try.", source: 'Beverly Sills' },
+  { quote: "I failed my way to success", source: 'Thomas Edison' },
+  { quote: "Many of life's failures are people who did not realize how close they were to success when they gave up.", source: 'Thomas A. Edison' },
+  { quote: "May you live all the days of your life.", source: 'Jonathan Swift' }
+];
 
 
 
