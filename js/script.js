00dab81d5350f955c1999e4d72b4d22acb0bf3c9
Troy Coffindaffer <61867285+tcatt71@users.noreply.github.com>
2022-09-19 00:50:48 -0700

    Update comments

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ project 1 - A Random Quote Generator
 // Check the "Project Resources" section of the project instructions
 // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
-/** An Array of qoute objects. */
+/** Array of qoute objects. */
 const quotes = [
   { quote: "Whatever the mind of man can conceive and believe, it can achieve.", source: 'Napoleon Hill', citation: 'Think and Grow Rich', year: 1937 },
   { quote: "You may be disappointed if you fail, but you are doomed if you don't try.", source: 'Beverly Sills' },
@@ -18,7 +18,7 @@ const quotes = [
 
 /**
  * Returns a random quote object from the quotes array.
- * @returns {object} The random quote object.
+ * @returns {object} A random quote object.
  */
 function getRandomQuote() {
   const randomIndexNumber = Math.floor(Math.random() * quotes.length)
